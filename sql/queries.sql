7e7fcd46b0d256ae86145c20a02ef65d05c82f8b
KV-prog-arch <koenvrijhoeven@gmail.com>
2026-09-18 15:30:34 +0200

    Update queries.sql no spelling mistakes

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,19 +1,19 @@
 -- One of the queries we used was finding the average recycling efficiency rate per country, ranked from highest to lowest. 
--- And if we know which coutries are better at recycling on average we can identify what the better strategies are, since there are multiple ones. 
+-- And if we know which countries are better at recycling on average we can identify what the better strategies are, since there are multiple ones. 
 SELECT c.country_name, AVG(rc.recycling_efficiency_rate) AS avg_efficiency -- selects country name and calculates the average recycling efficiiency 
-FROM country c -- start from the country table and c becomes the alies 
+FROM country c -- start from the country table and c becomes the alias 
 JOIN recycling_company rc ON c.country_id = rc.country_id -- country <-> recycling_company 
 GROUP BY c.country_id, c.country_name -- turns these rows in one group 
 ORDER BY avg_efficiency DESC;  -- this gives us the order of average efficiency of each country from highest to lowest 
 
--- The other query that we used was "WHERE NOT EXISTS" to see which if there exists suppliers that do NOT use any extraction method, and therefore have the highest environmental risk rating (5)
+-- The other query that we used was "WHERE NOT EXISTS" to see if there exists suppliers that do NOT use any extraction method, and therefore have the highest environmental risk rating (5)
 -- This is important for our database to recognize companies that don't use the highest risk level, and invest in them, because it has less of a negative effect on the envriontment.
 SELECT s.supplier_id, s.supplier_name, c.country_name  -- picks out supplier id, name and country name 
-FROM supplier s -- starts from supplier country and s becomes the  alies 
+FROM supplier s -- starts from supplier country and s becomes the  alias 
 JOIN country c ON s.country_id = c.country_id -- country <-> supplier
 WHERE NOT EXISTS (        -- keeps this supplier only if subquery below finds NOTHING 
     SELECT 1              -- placeholder to check existence 
-    FROM extraction e      -- starts from extraction, e becomes the alies 
+    FROM extraction e      -- starts from extraction, e becomes the alias 
     JOIN extraction_method em ON e.extraction_method_id = em.extraction_method_id -- extraction <-> extraction_method 
     WHERE e.supplier_id = s.supplier_id         -- match extractions to specific supplier 
     AND em.environmental_risk_rating = 5        -- checks if any of them is rated the highest 
@@ -23,7 +23,7 @@ WHERE NOT EXISTS (        -- keeps this supplier only if subquery below finds NO
 -- And this is important, because the highest recovery rate percentage, tells us which country is the best at recovering finite materials. So we can see what methods they use and try to implement them in countries 
 -- with for example a very low recovery rate percentage. 
 SELECT wr.waste_record_id, pt.product_type_name, c.country_name, r.recover_rate_percent -- picks waste record, product type, country and recovery rate 
-FROM waste_record wr -- starts from waste_record and wr becomes the alies 
+FROM waste_record wr -- starts from waste_record and wr becomes the alias 
 JOIN product_type pt ON wr.product_type_id = pt.product_type_id -- product_type <-> waste_record
 JOIN country c ON wr.country_id = c.country_id -- country <-> waste_record
 JOIN recovery r ON wr.waste_record_id = r.waste_record_id -- recovery <-> waste_record
