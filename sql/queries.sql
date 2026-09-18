ada5bdef252a0bac1823d1cd9d215980a28ed2ec
KV-prog-arch <koenvrijhoeven@gmail.com>
2026-09-18 16:54:27 +0200

    Update queries.sql fixed comments

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,13 +1,16 @@
 -- One of the queries we used was finding the average recycling efficiency rate per country, ranked from highest to lowest. 
 -- And if we know which countries are better at recycling on average we can identify what the better strategies are, since there are multiple ones. 
-SELECT c.country_name, AVG(rc.recycling_efficiency_rate) AS avg_efficiency -- selects country name and calculates the average recycling efficiiency 
+SELECT c.country_name, AVG(rc.recycling_efficiency_rate) AS avg_efficiency -- selects country name and calculates the average recycling efficiency 
 FROM country c -- start from the country table and c becomes the alias 
 JOIN recycling_company rc ON c.country_id = rc.country_id -- country <-> recycling_company 
 GROUP BY c.country_id, c.country_name -- turns these rows in one group 
 ORDER BY avg_efficiency DESC;  -- this gives us the order of average efficiency of each country from highest to lowest 
 
--- The other query that we used was "WHERE NOT EXISTS" to see if there exists suppliers that do NOT use any extraction method, and therefore have the highest environmental risk rating (5)
--- This is important for our database to recognize companies that don't use the highest risk level, and invest in them, because it has less of a negative effect on the envriontment.
+
+-- The other query that we used was "WHERE NOT EXISTS" to remove suppliers that use an extraction method with the highest environmental risk rating (5).
+-- This means that the results only include suppliers that do not have an extraction method with a risk rating of 5, so all lower-risk suppliers remain.
+-- Because MySQL does not have a "minus" operation for this purpose, we use "WHERE NOT EXISTS" to exclude the suppliers with the highest risk rating.
+-- This is important for our database to recognize companies that don't use the highest risk level, and invest in them, because it has less of a negative effect on the environment.
 SELECT s.supplier_id, s.supplier_name, c.country_name  -- picks out supplier id, name and country name 
 FROM supplier s -- starts from supplier country and s becomes the  alias 
 JOIN country c ON s.country_id = c.country_id -- country <-> supplier
